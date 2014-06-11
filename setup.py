5afb0572218839474a5746082b8ed040fb06604c
Petter Strandmark <petter.strandmark@gmail.com>
2014-06-11 18:41:55 +0200

    Documentation string in the beginning.

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+"""
+Just install with
 
+	pip install numpy_display
+
+and then type the following in your IPython notebook:
+
+	import numpy_display
+
+Then your numpy matrices will be formatted as HTML tables.
+"""
 from setuptools import setup
 setup(
 	author="Petter Strandmark",
